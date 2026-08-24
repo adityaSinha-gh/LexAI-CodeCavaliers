d41517628bf6064e49e2624b2176991d2a08bf4d
somesh dewangan <someshdew30062020@gmail.com>
2026-08-24 23:04:21 +0530

    added the JWT key

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -6,14 +6,14 @@ if(!process.env.MONGO_URI){
     throw new Error("file does not exist");
 
 }
-if(!process.env.PORT){
+if(!process.env.JWT_SECRET){
     throw new Error("Port does not exist")
 }
 
 const config = {
     MONGO_URI :process.env.MONGO_URI,
     JWT_SECRET:process.env.JWT_SECRET,
-    PORT:process.env.PORT
+    
 }
 
 
